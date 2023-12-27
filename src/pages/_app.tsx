9860366acb09b5b6ca2cb3293c821fc149f08b6c
Martins <olamideakeredolu@gmail.com>
2023-12-27 19:36:55 +0100

    chore: updating how font is being fetched to prevent nextjs error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,20 @@ import 'styles/customcalendarstyles.scss';
 import 'styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <link
+          href='https://fonts.cdnfonts.com/css/euclid-circular-b'
+          rel='stylesheet'
+        />
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
